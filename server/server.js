84a17915229e51ab978c6a4f790328c5fa359087
Abhishek Patel <71484679+abhiishek-10@users.noreply.github.com>
2024-06-04 12:11:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config({
   // path: path.join(__dirname, `/env/${process.env.NODE_ENV}.env`),
-  path: path.join(__dirname, `./.env`),
+  path: path.join(__dirname, `/.env`),
 });
 
 // Connect to MongoDB
